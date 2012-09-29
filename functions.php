5bcde215794e6f064310d43610428551323d58e3
Taras Mankovski <tarasm@gmail.com>
2012-09-29 18:24:20 -0400

    Added Private title of the theme

--- functions.php
+++ functions.php
@@ -1,15 +1,12 @@
 <?php
 
-/**
-   * Register with hook 'wp_enqueue_scripts', which can be used for front end CSS and JavaScript
-   */
-  add_action( 'wp_enqueue_scripts', 'wrktg_enqueue_print_css' );
+add_action( 'wp_enqueue_scripts', 'wrktg_enqueue_print_css' );
 
-  /**
-   * Enqueue plugin style-file
-   */
-  function wrktg_enqueue_print_css() {
-      // Respects SSL, Style.css is relative to the current file
-      wp_register_style( 'wrktg-print-css', get_stylesheet_directory_uri() . '/print.css', null, null, 'print' );
-      wp_enqueue_style( 'wrktg-print-css' );
-  }
+/**
+* Enqueue plugin style-file
+*/
+function wrktg_enqueue_print_css() {
+  // Respects SSL, Style.css is relative to the current file
+  wp_register_style( 'wrktg-print-css', get_stylesheet_directory_uri() . '/print.css', null, null, 'print' );
+  wp_enqueue_style( 'wrktg-print-css' );
+}
