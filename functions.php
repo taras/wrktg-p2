5f4f76812675038f27ed24188932982d7bf87afa
Taras Mankovski <tarasm@gmail.com>
2012-09-30 11:56:13 -0400

    Added Public Sidebars

--- functions.php
+++ functions.php
@@ -10,3 +10,29 @@ function wrktg_enqueue_print_css() {
   wp_register_style( 'wrktg-print-css', get_stylesheet_directory_uri() . '/print.css', null, null, 'print' );
   wp_enqueue_style( 'wrktg-print-css' );
 }
+
+function public_widgets_init() {
+
+    register_sidebar(array(
+        'name' => __('Public - Top Widget Area', 'wrktg-p2'),
+        'id' => 'public_top',
+        'description' => __('This widget area shows up in header on public theme.', 'wrktg-p2'),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s group">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Public - Sidebar Widget Area', 'wrktg-p2'),
+        'id' => 'public_side',
+        'description' => __('This widget area shows up in the sidebar on public theme.', 'wrktg-p2'),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s group">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
+
+}
+
+add_action('widgets_init', 'public_widgets_init');
